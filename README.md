bbd1c96fb60c8ea148ff76a5db4f6e47c01d3825
jacobNar <jdnarayan56@gmail.com>
2021-10-24 23:03:36 -0500

    updating readme and package.json with new name

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AmazonScraper
+# CdkeysScraper
+
 A web scraper, that will check the prices of items for you
--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const vgmUrl= 'https://www.cdkeys.com/pc';
   const response = await got(vgmUrl);
   const $ = cheerio.load(response.body);
 
-  var productNames = $('a.product-item-link')
-  var productPrices = $('span.price')
-  var oldPrices = $('span.old-price')
+  var oldPrices = $('span[data-price-type="oldPrice"] span')
   var products = $("li.product-item")
-  // console.log(`product names: ${productNames.length} product prices: ${productPrices.length} old prices: ${oldPrices.length}`)
-  console.log(products[0])
+
   products.each((i, product) => {
     // const href = link.text();
     prod = JSON.parse(product.attribs['data-impression'])
 
-    console.log((prod.name + " - $" + prod.price));
-    // console.log(`${product.attribs['data-imppression']['name']} - ${product.attribs['data-imppression']['price']}`)
+    console.log((prod.name + " - $" + prod.price + " - " + oldPrices[i].children[0].data + "\n"));
+
   });
 })();
